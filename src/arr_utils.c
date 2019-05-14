d5745f3ab7d219c1527624c14137ef990118136b
Camille Louis <camille.louis@epita.fr>
2019-05-14 13:00:54 +0200

    utils for array manipulation, strcat and strncpy

---
+++ src/arr_utils.c
@@ -0,0 +1,42 @@
+#include "arr_utils.h"
+
+char* my_strncpy(char* str, int len)
+{
+    char* res = malloc(len);
+    if (!res)
+        return NULL;
+    strncpy(res, str, len);
+    res[len] = '\0';
+    return res;
+}
+
+void my_strcat(char* str1, char* str2)
+{
+    str1 = realloc(str1, strlen(str1) + strlen(str2));
+    if (!str1)
+        return;
+    strcat(str1, str2);
+}
+
+char** arr_add(char* str, char** arr)
+{
+    if (!str)
+        return NULL;
+    if (arr == NULL)
+    {
+        arr = malloc(strlen(str) + sizeof(NULL));
+        arr[0] = str;
+        arr[1] = NULL;
+    }
+    else
+    {
+        int len = 0;
+        int i = 0;
+        for (; arr[i] != NULL; i++)
+            len += strlen(arr[i]);
+        arr = realloc(arr, len + strlen(str));
+        arr[i] = str;
+        arr[i + 1] = NULL;
+    }
+    return arr;
+}
---
+++ src/arr_utils.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+
+char** arr_add(char* str, char** arr);
+char* my_strncpy(char* str, int len);
+void my_strcat(char* str1, char* str2);
